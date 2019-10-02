2945fbe4be5f4a870c650d306231f3c9575a4c36
OBAID ALQAHTANI <obal3588@colorado.edu>
2019-10-02 23:03:38 +0300

    part 7 -10

--- homework.js
+++ homework.js
@@ -61,3 +61,72 @@ const countUpAndDown =function(num){
     }
     return result
 }
+
+/*part 7*/
+const wordsWithA = function(arr){
+    let result  =[];
+    let expr = /a/;
+    let i=0;
+    while (i<arr.length){
+        if (arr[i].search(expr)==1)
+            result.push(arr[i])
+        i++;
+    }
+    return result 
+}
+
+///*part 8*//
+const wordsWithLetter = function(letter, arr){
+    let result  =[];
+    let temp = letter ;
+    let expr = new RegExp(temp);
+  //console.log(expr)
+    let i=0;
+    while (i<arr.length){
+    //  console.log(arr[i].search(expr))
+        if (arr[i].search(expr)!=-1)
+            result.push(arr[i])
+        i++;
+    }
+    return result 
+}
+
+/***part 9 */
+
+const longestWord = function(text){
+    const string_arr = text.split(" ");
+      let i=1;
+      let max_length=0;
+      while(i<string_arr.length){
+          if ((string_arr[i].length) > (string_arr[i-1].length))
+              max_length=i;
+            i++;
+      
+      }
+      return string_arr[max_length];
+  }
+  
+
+/*part 10 */
+
+const largestEvenNumber =function(arr_one ){
+    
+    let arr =[]
+    let j =0
+    
+    while (j<arr_one.length){
+        if (arr_one[j]%2==0)
+            arr.push(arr_one[j])
+        j++
+    }
+
+    let max_value = arr[0];
+    let i =1
+    while (i<arr.length){
+        if (arr[i]> max_value)
+            max_value=arr[i]
+        i++;
+    }
+    return max_value
+}
+
