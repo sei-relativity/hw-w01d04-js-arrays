1760fa0ec5231415e3a59422fd419c348731c856
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-02 16:26:23 +0300

    solution to 1

---
+++ homework.js
@@ -0,0 +1,20 @@
+// 1
+const summation = function (n) { 
+    let sum = 0;
+    for (let i = 1; i <= n; i++){
+        sum += i
+    }
+    return sum
+}
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+
+
