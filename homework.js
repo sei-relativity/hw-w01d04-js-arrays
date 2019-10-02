420b0d4a6c52bcf72ef08d5dbff821a55be2312c
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-02 16:42:40 +0300

    solution to 2

--- homework.js
+++ homework.js
@@ -2,11 +2,21 @@
 const summation = function (n) { 
     let sum = 0;
     for (let i = 1; i <= n; i++){
-        sum += i
+        sum += i;
     }
-    return sum
+    return sum;
 }
+
 // 2
+const summationEven = function ([arr]) {
+    let evens = [];
+    for (let i = 0; index <= arr.length; index++) {
+        if (arr[i]%2==0){
+            arr.push(i);
+        }
+        return evens;
+    }
+}
 // 3
 // 4
 // 5
