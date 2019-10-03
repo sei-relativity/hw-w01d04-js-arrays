03c44608debba5fe0431bb95eec25f628a493a90
aba20 <askr1000@gmail.com>
2019-10-03 08:38:55 +0300

    Abdulrahmna Alfouzan HW.04

---
+++ homework.js
@@ -0,0 +1,30 @@
+const n = [1, 2, 3, 4, 5]
+const summ = function sumArr(n){
+    for (i = 0; n, i)
+        return n + i
+    })
+
+}
+
+let numArr = [1, 2, 3, 4, 5, 6];
+const x = function(numArr){
+    for (numArr %2 !==0){
+
+    }
+}
+
+// #3
+
+let num = [1, 2, 3, 4, 5];
+
+let maxElement = -1
+const summ = function sumArr(n){
+    for(i = 0; n.length; i++) {
+      if (n[i] > maxElement){
+        maxElement = n[i];
+      }
+      
+    }
+}
+
+console.log(summ(num))
